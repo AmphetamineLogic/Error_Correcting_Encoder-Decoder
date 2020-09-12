d480134c90cf64788dec36c88c5b234c0a377ce6
Rabblerouser <phoenix.public@gmail.com>
2020-09-12 22:19:23 +0300

    Stage 2 completed

--- Error Correcting Encoder-Decoder/task/src/correcter/Main.java
+++ Error Correcting Encoder-Decoder/task/src/correcter/Main.java
@@ -7,7 +7,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        System.out.println(makeErrors(input));
+        String trebled = treble(input);
+        String withErrors = makeErrors(trebled);
+        String corrected = corrected(withErrors);
+
+        System.out.println(input);
+        System.out.println(trebled);
+        System.out.println(withErrors);
+        System.out.println(corrected);
     }
 
     private static String makeErrors (String input) {
@@ -21,7 +28,7 @@ public class Main {
             while (positionToChange > chars.length) {
                 positionToChange--;
             }
-            
+
             characterToInsert = alphabet.charAt(random.nextInt(alphabet.length()));
 
             while (chars[positionToChange] == characterToInsert) {
@@ -30,5 +37,32 @@ public class Main {
             chars[positionToChange] = characterToInsert;
         }
         return new String(chars);
+    }
+
+    private static String treble (String input) {
+        char[] chars = input.toCharArray();
+        char[] result = new char[chars.length * 3];
+        for (int i = 0; i < chars.length; i++) {
+            result[i * 3] = chars[i];
+            result[i * 3 + 1] = chars[i];
+            result[i * 3 + 2] = chars[i];
+        }
+        return new String(result);
+    }
+
+    private static String corrected (String input) {
+        char[] chars = input.toCharArray();
+        char correct;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < chars.length; i += 3) {
+            if (chars[i] == chars[i + 1] || chars[i] == chars[i + 2]) {
+                correct = chars[i];
+            }
+            else {
+                correct = chars[i+1];
+            }
+            stringBuilder.append(correct);
+        }
+        return stringBuilder.toString();
     }
 }
