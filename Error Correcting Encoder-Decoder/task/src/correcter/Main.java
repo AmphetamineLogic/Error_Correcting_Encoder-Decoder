9a4b11346837c5f5700a948ca416a02adbc824aa
Rabblerouser <phoenix.public@gmail.com>
2020-09-29 17:17:17 +0300

    Stage 3 completed

--- Error Correcting Encoder-Decoder/task/src/correcter/Main.java
+++ Error Correcting Encoder-Decoder/task/src/correcter/Main.java
@@ -1,68 +1,47 @@
 package correcter;
 
+import java.io.*;
+import java.util.Arrays;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        String trebled = treble(input);
-        String withErrors = makeErrors(trebled);
-        String corrected = corrected(withErrors);
-
-        System.out.println(input);
-        System.out.println(trebled);
-        System.out.println(withErrors);
-        System.out.println(corrected);
+        String sendPath = "send.txt";
+        String receivedPath = "received.txt";
+        writeFile(makeErrors(readFile(sendPath)), receivedPath);
     }
 
-    private static String makeErrors (String input) {
+    private static byte[] readFile (String fileName) {
+        byte[] result = new byte[0];
+        try {
+            FileInputStream fileInputStream = new FileInputStream(fileName);
+            try (BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream)) {
+                result = bufferedInputStream.readAllBytes();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private static void writeFile (byte[] bytesWithErrors, String fileName) {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+            fileOutputStream.write(bytesWithErrors);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static byte[] makeErrors (byte[] sourceBytes) {
         Random random = new Random();
-        char[] chars = input.toCharArray();
-        String alphabet = " 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        int positionToChange;
-        char characterToInsert;
-        for (int i = 0; i < chars.length; i += 3) {
-            positionToChange = i + random.nextInt(3);
-            while (positionToChange > chars.length) {
-                positionToChange--;
-            }
-
-            characterToInsert = alphabet.charAt(random.nextInt(alphabet.length()));
-
-            while (chars[positionToChange] == characterToInsert) {
-                characterToInsert = alphabet.charAt(random.nextInt(alphabet.length()));
-            }
-            chars[positionToChange] = characterToInsert;
+        byte bitToChange;
+        for (int i = 0; i < sourceBytes.length; i++) {
+            bitToChange = (byte) random.nextInt(8);
+            sourceBytes[i] ^= 1 << bitToChange;
         }
-        return new String(chars);
+        return sourceBytes;
     }
 
-    private static String treble (String input) {
-        char[] chars = input.toCharArray();
-        char[] result = new char[chars.length * 3];
-        for (int i = 0; i < chars.length; i++) {
-            result[i * 3] = chars[i];
-            result[i * 3 + 1] = chars[i];
-            result[i * 3 + 2] = chars[i];
-        }
-        return new String(result);
-    }
 
-    private static String corrected (String input) {
-        char[] chars = input.toCharArray();
-        char correct;
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < chars.length; i += 3) {
-            if (chars[i] == chars[i + 1] || chars[i] == chars[i + 2]) {
-                correct = chars[i];
-            }
-            else {
-                correct = chars[i+1];
-            }
-            stringBuilder.append(correct);
-        }
-        return stringBuilder.toString();
-    }
 }
